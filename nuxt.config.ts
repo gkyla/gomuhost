a06b4c8d8cd51edfc2228cdaebb4b416bb833b5e
gkyla <agitsyang@gmail.com>
2023-03-11 17:43:59 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/eslint-module',
+    'nuxt-headlessui'
+  ]
+})
