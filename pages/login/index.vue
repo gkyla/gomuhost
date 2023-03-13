c3eb7b745cebc47e985329ea2d95dd4b665228a6
gkyla <agitsyang@gmail.com>
2023-03-13 16:52:35 +0700

    feat: add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form>
+      <div>
+        <label for="username"></label>
+        <input type="text" id="username" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
